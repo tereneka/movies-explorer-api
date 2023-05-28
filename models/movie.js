0b1f86c4a6a7c9de46b4b3a138336891f81fec8a
tereneka <teren-eka@yandex.ru>
2023-05-28 17:30:46 +0300

    feat: create schemas and models

---
+++ models/movie.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const { Schema } = mongoose;
+
+const urlValidator = {
+  validator(v) {
+    return validator.isURL(v, {
+      protocols: ['http', 'https'],
+      require_protocol: true,
+    });
+  },
+  message: (props) => `${props.value} - некорректный url!`,
+};
+
+const movieSchema = new Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: urlValidator,
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: urlValidator,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: urlValidator,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
