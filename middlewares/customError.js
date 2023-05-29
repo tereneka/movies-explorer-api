df980986e39ae94ad7c430ed70e3eca4faafef18
tereneka <teren-eka@yandex.ru>
2023-05-29 20:31:15 +0300

    не работает

--- middlewares/customError.js
+++ middlewares/customError.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const { Error } = mongoose;
 const BadRequestErr = require('../errors/badRequestErr');
 const ConflictErr = require('../errors/conflictErr');
-const { errMessages } = require('../utils/errStatus');
+const { errMessage } = require('../constants');
 
 module.exports = (err, req, res, next) => {
   if (err instanceof Error.CastError || err instanceof Error.ValidationError) {
-    next(new BadRequestErr(errMessages.BAD_REQUEST));
+    next(new BadRequestErr(errMessage.BAD_REQUEST));
   } else if (err.code === 11000) {
-    next(new ConflictErr(errMessages.CONFLICT));
+    next(new ConflictErr(errMessage.CONFLICT));
   } else {
     next(err);
   }
